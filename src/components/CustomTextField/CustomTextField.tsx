196a73787902fad27feb88d51574b2b78b6d509c
Andrew Smirnov <smirnov@brick.dev>
2022-12-24 19:51:06 +0300

    Revert "Add CustomTextField component"
    This reverts commit c3356d71d10ae8e299303a18989c9003987ff1b4.

--- src/components/CustomTextField/CustomTextField.tsx
+++
@@ -1,5 +0,0 @@
-import { TextField, TextFieldProps } from '@mui/material';
-
-export default function CustomTextField(props: TextFieldProps) {
-  return <TextField {...props} margin="normal" fullWidth />;
-}
