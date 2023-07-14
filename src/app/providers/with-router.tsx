49b6fc06281e826311cac3b2ac1e62dd6a3853c4
Vladimir <v.parasochka@bftcom.com>
2023-07-14 13:42:29 +0300

    refactor(no-ref): Рефакторинг провайдеров

--- src/app/providers/with-router.tsx
+++ src/app/providers/with-router.tsx
@@ -2,12 +2,11 @@
 import { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-export const withRouter = (component: () => React.ReactNode) =>
-  // eslint-disable-next-line react/display-name, func-names
-  function () {
-    return (
-      <BrowserRouter>
-        <Suspense fallback="Загрузка...">{component()}</Suspense>
-      </BrowserRouter>
-    );
-  };
+// eslint-disable-next-line react/display-name, func-names
+export const withRouter = (component: () => React.ReactNode) => () => {
+  return (
+    <BrowserRouter>
+      <Suspense fallback="Загрузка...">{component()}</Suspense>
+    </BrowserRouter>
+  );
+};
